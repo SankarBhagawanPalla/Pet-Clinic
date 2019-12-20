1ddcf263cf7b4ca674b9571b2bb17545a272cc22
SankarBhagawanPalla <54730150+SankarBhagawanPalla@users.noreply.github.com>
2019-12-20 18:16:36 -0500

    Initial Project Add

---
+++ src/main/java/com/springworks/sanpetclinic/SanPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.springworks.sanpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springworks/sanpetclinic/SanPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springworks.sanpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
