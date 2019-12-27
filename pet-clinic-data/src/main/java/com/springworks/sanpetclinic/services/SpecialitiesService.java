0916c2941a1c333fbb281826c9587e6861ea879f
SankarBhagawanPalla <54730150+SankarBhagawanPalla@users.noreply.github.com>
2019-12-27 16:34:41 -0500

    Implemented Speciality Map service.Closes #25

---
+++ pet-clinic-data/src/main/java/com/springworks/sanpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.springworks.sanpetclinic.services;
+
+import com.springworks.sanpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
